e5e30c51741e73f750929f1e46a48befd3949fb0
Ivaylo37 <ivailo.vassilev99@gmail.com>
2024-08-16 01:34:08 +0300

    Modified the echange request and statistics models as well

--- src/main/java/com/example/egt/models/ExchangeRequest.java
+++ src/main/java/com/example/egt/models/ExchangeRequest.java
@@ -1,10 +1,13 @@
 package com.example.egt.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
@@ -12,13 +15,27 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Component
+@Entity
+@Table(name = "exchange_requests")
 public class ExchangeRequest {
 
+    @Id
+    @Column(name = "request_id")
     private String requestId;
+
+    @Column(nullable = false)
     private LocalDateTime timestamp;
+
+    @Column(nullable = false)
     private String client;
+
+    @Column(name = "base_currency", nullable = false, length = 3)
     private String baseCurrency;
+
+    @Column(name = "target_currency", nullable = false, length = 3)
     private String targetCurrency;
-    private Integer period;
+
+    @Column
+    private Integer period; // for history requests
+
 }
--- src/main/java/com/example/egt/models/Statistics.java
+++ src/main/java/com/example/egt/models/Statistics.java
@@ -1,10 +1,10 @@
 package com.example.egt.models;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
@@ -12,12 +12,22 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Component
+@Entity
+@Table(name = "statistics")
 public class Statistics {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "service_name", nullable = false)
     private String serviceName;
+
+    @Column(name = "request_id", nullable = false)
     private String requestId;
+
+    @Column(nullable = false)
     private LocalDateTime timestamp;
+
+    @Column(name = "end_client_id", nullable = false)
     private String endClientId;
 }
