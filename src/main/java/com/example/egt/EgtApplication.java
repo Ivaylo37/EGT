ea3dcac1195d1c81438d985d9f24caca83568a44
Ivaylo37 <ivailo.vassilev99@gmail.com>
2024-08-18 15:40:29 +0300

    Configured Swagger

--- src/main/java/com/example/egt/EgtApplication.java
+++ src/main/java/com/example/egt/EgtApplication.java
@@ -3,10 +3,12 @@ package com.example.egt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableCaching
-public class EgtApplication {
+@EnableSwagger2
+public class EgtApplication {// http://localhost:8080/swagger-ui/
 
 	public static void main(String[] args) {
 		SpringApplication.run(EgtApplication.class, args);
